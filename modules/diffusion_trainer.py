b3d9104c86f10e2571cb3270b9d5094cb20351f6
filippo <filipposchiazza97@gmail.com>
2024-02-01 17:05:42 +0100

    Add latent diffusion trainer

--- modules/diffusion_trainer.py
+++ modules/diffusion_trainer.py
@@ -37,7 +37,8 @@ class DiffusionTrainer():
             self.unet.train()
 
             # Train one epoch
-            train_loss = self._train_one_epoch()
+            train_loss = self._train_one_epoch(train_dataloader=train_dataloader,
+                                               epoch=epoch)
 
             # Update history
             history['loss_train'].append(train_loss)
@@ -46,7 +47,7 @@ class DiffusionTrainer():
             self.unet.eval()
 
             # Validate one epoch
-            val_loss = self._validate()
+            val_loss = self._validate(val_dataloader=val_dataloader)
 
             # Update history
             history['loss_val'].append(val_loss)
@@ -147,6 +148,122 @@ class DiffusionTrainer():
 
 
 
+
+
+class LatentDiffusionTrainer(DiffusionTrainer):
+
+    def __init__(self, 
+                 unet, 
+                 pretrained_module,
+                 gdf_util, 
+                 optimizer, 
+                 device, 
+                 verbose=True):
+        super().__init__(unet, gdf_util, optimizer, device, verbose)
+        self.pretrained_module = pretrained_module.to(device)
+        self.pretrained_module.eval()
+
+
+
+    def _train_one_epoch(self,
+                         train_dataloader,
+                         epoch):
+        
+        running_loss = 0.
+        mean_loss = 0.
+
+        with tqdm(train_dataloader, unit="batches") as tepoch:
+
+            for batch_idx, imgs in enumerate(tepoch):
+
+                # Update the progress bar description
+                tepoch.set_description(f'Epoch {epoch+1}')
+
+                # Load images on device
+                imgs = imgs.to(device=self.device, dtype=torch.float)
+
+                # encode imgs and get codes
+                with torch.no_grad():
+                    e = self.pretrained_module.encoder(imgs)
+                    _, _, _, _, codes = self.pretrained_module.vq_layer(e)
+                    codes.squeeze_(1)  # add channel dimension
+                    codes = codes * 2 / (self.pretrained_module.num_emb - 1) - 1.0    # from [0, num_emb - 1] to [-1, 1]
+
+                # 1. Get the batch size
+                batch_size = codes.shape[0]
+        
+                # 2. Sample timesteps uniformely
+                t = torch.randint(low=0, high=self.timesteps, size=(batch_size, )).to(self.device)
+        
+                # 3. Sample random noise to be added to the images in the batch
+                noise = torch.randn(size=codes.shape, dtype=imgs.dtype).to(self.device)
+
+                # 4. Diffuse the images with noise
+                imgs_t = self.gdf_util.q_sample(codes, t, noise).to(self.device, dtype=torch.float)
+        
+                # 5. Pass the diffused images and time steps to the unet
+                pred_noise = self.unet((imgs_t, t))
+                
+                # 6. Calculate the loss and update the weights
+                loss = F.mse_loss(noise, pred_noise)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+            
+                # 7. Update running losses and mean losses
+                running_loss += loss.item()
+                mean_loss = running_loss / (batch_idx + 1)
+            
+                tepoch.set_postfix(loss="{:.6f}".format(mean_loss))
+        
+        return mean_loss
+
+
+
+
+    def _validate(self, val_dataloader):
+
+        running_val_loss = 0.
+
+        with torch.no_grad():
+            for batch_idx, imgs in enumerate(val_dataloader):
+
+                # Load images on device
+                imgs = imgs.to(device=self.device, dtype=torch.float)
+
+                # encode imgs and get codes
+                e = self.pretrained_module.encoder(imgs)
+                _, _, _, _, codes = self.pretrained_module.vq_layer(e)
+                codes.squeeze_(1)  # add channel dimension
+                codes = codes * 2 / (self.pretrained_module.num_emb - 1) - 1.0    # from [0, num_emb - 1] to [-1, 1]
+
+                # 1. Get the batch size
+                batch_size = codes.shape[0]
+        
+                # 2. Sample timesteps uniformely
+                t = torch.randint(low=0, high=self.timesteps, size=(batch_size, )).to(self.device)
+        
+                # 3. Sample random noise to be added to the images in the batch
+                noise = torch.randn(size=codes.shape, dtype=imgs.dtype).to(self.device)
+
+                # 4. Diffuse the images with noise
+                imgs_t = self.gdf_util.q_sample(codes, t, noise).to(self.device, dtype=torch.float)
+        
+                # 5. Pass the diffused images and time steps to the unet
+                pred_noise = self.unet((imgs_t, t))
+                
+                # 6. Calculate the loss 
+                loss = F.mse_loss(noise, pred_noise)
+            
+                # 7. Update running losses and mean losses
+                running_val_loss += loss.item()
+
+        mean_val_loss = running_val_loss / len(val_dataloader)
+
+        if self.verbose == True:
+            print(f"Validation loss: {mean_val_loss:.6f}")
+
+        return mean_val_loss
 
 
 """
